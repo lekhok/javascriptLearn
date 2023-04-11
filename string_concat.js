63f2d1ed32b0e13786091c4c9abb2e0f1b5841f4
Kumud Lekhok <kumudclekhok@gmail.com>
2023-04-12 01:46:05 +0530

    Basic file for jest testing

---
+++ string_concat.js
@@ -0,0 +1,7 @@
+function concatStr (StrA, StrB){
+    console.log( StrA + StrB);
+}
+
+
+concatStr ("abc", "def");
+concatStr (1, 2); //expected to be concated as strings but being passed as numbers to the function
