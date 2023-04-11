b37f4dac413ae6d728e970aab636524776d1b97a
Kumud Lekhok <kumudclekhok@gmail.com>
2023-04-12 02:20:22 +0530

    Initial commit for learning and testing using jest

--- addFive.test.js
+++ addFive.test.js
@@ -2,6 +2,9 @@ const { test } = require('node:test');
 const addFive = require('./addFive');
 const { hasUncaughtExceptionCaptureCallback } = require('process');
 
+
+// Figure out the testing method and what is going wrong 
+ 
 test('returns the number plus 5', () => {
     hasUncaughtExceptionCaptureCallback
 }
