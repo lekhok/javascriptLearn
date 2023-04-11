fbb4e444a85ffd8e5d15b4655f2868efd2ef3973
Kumud Lekhok <kumudclekhok@gmail.com>
2023-04-12 01:54:46 +0530

    working on a different function file

--- string_concat.js
+++
@@ -1,7 +0,0 @@
-function concatStr (StrA, StrB){
-    console.log( StrA + StrB);
-}
-
-
-concatStr ("abc", "def");
-concatStr (1, 2); //expected to be concated as strings but being passed as numbers to the function
